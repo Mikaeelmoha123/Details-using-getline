aeb2be42f101c2ab091cddd0453ae5acf11fb393
Mikaeel <95021467+Mikaeelmoha123@users.noreply.github.com>
2022-11-12 00:44:30 -0800

    Add files via upload
    you can key in as many foods and movies as you feel like since i used the getline operator

---
+++ detrails output.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+int main()
+{
+    string FirstName, LastName, FullName, FavoriteMeal, FavoriteMovie;
+    cout << "Your First Name: ";
+    cin >> FirstName;
+    cout << "Your Last Name: ";
+    cin >> LastName;
+    FullName = FirstName + LastName;
+    cout << "Your Full Name: " << FullName << "\n";
+    getline(cin, FullName);
+    cout << "Your Favorite Meal: ";
+    getline(cin, FavoriteMeal);
+    cout << "Your Favorite Movie: ";
+    getline(cin, FavoriteMovie);
+}
